5014157825874a46be0bbf4852c2730d6863948f
Adolfo Jarsún <ajarsun@hotmail.com>
2023-03-03 18:06:00 -0400

    First Version

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('principal.html', message='Hello, World!')
+
 ## Modificación de Prueba
 if __name__ == '__main__':
     app.run(debug=True)
